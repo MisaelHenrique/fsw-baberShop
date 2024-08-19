1f7c39cce08e3d505d93318a4cb485e48f8d8617
Misael <misaelhenriquesilva@gmail.com>
2024-08-18 20:13:11 -0400

    feat: add BookingItem component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,9 @@ import { AvatarImage, Avatar } from "./_components/ui/avatar";
 import { Badge } from "./_components/ui/badge";
 import { db } from "./_lib/prisma";
 import BarberShopItem from "./_components/barbershop-item";
+import { quickSearchOptions } from "./_constants/search";
+import BookingItem from "./_components/booking-item";
+
 
 const Home =  async () => {
   // chamar o banco de dados
@@ -40,32 +43,17 @@ const Home =  async () => {
 
         {/*Busca Rápida*/}
         <div className="mt-6 flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
-
-          <Button className="gap-2" variant="secondary">
-            < Image
-            src="/cabelo.svg" width={16} height={16}
-            alt="Cabelo"
-            />
-            Cabelo
+          {quickSearchOptions.map((option) => (
+            <Button key={option.title} variant="secondary" className="gap-2">
+              <Image src={option.imageUrl} width={16} height={16} alt={option.title}/>
+              {option.title}
             </Button>
+          ))}
 
-          <Button className="gap-2" variant="secondary">
-            <Image src="/barba.svg" width={16} height={16}
-            alt="Barba"/>  
-            Barba
-            </Button>
-          
-
-          <Button className="gap-2" variant="secondary">
-            <Image
-              src="/acabamento.svg" width={16} height={16}
-              alt="acabamento"
-            />
-            Acabamento
-            </Button>
+            
         </div>
 
-        {/*Banner*/}
+        {/*IMAGEM*/}
         <div className="relative w=full h-[150px] mt-6">
           <Image  
           alt="Agende nos melhores com a Barber Shop"
@@ -75,33 +63,8 @@ const Home =  async () => {
           />
         </div>
 
-        {/*AGENDAMENTOS*/}
-        <h2 className="mt-6 mb-3 text-xs font-bold uppercase text-gray-400">
-          Agendamentos
-        </h2>
-
-        <Card>
-          <CardContent className="flex justify-between p-0">
-            {/*ESQUEDA*/}
-            <div className="flex-col gap-2 py-5">
-              <Badge className="w-fit">Confirmado</Badge>
-              <h3 className="font-semibold">Cortes de Cabelo</h3>
-
-              <div className="flex items-center gap-2">
-                <Avatar className="w-6 h-6">
-                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" alt="Avatar"/>
-                </Avatar>
-                <p className="text-sm">Barbearia FSW</p>
-              </div>
-            </div>
-            {/*DIREITA*/}
-            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
-              <p className="text-sm">Agosto</p>
-              <p className="text-2xl">05</p>
-              <p className="text-sm">20:00</p>
-            </div>
-          </CardContent>
-        </Card>
+        {/*AGENDEMANTOS*/}
+        <BookingItem />
 
         <h2 className="mt-6 mb-3 text-xs font-bold uppercase text-gray-400">
           Agendamentos
