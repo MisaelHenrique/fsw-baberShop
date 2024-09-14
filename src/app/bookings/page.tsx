f1b1de162660c2e5908cc033aa676b455caff445
Misael <misaelhenriquesilva@gmail.com>
2024-09-13 23:45:55 -0400

    refactor:  add data functions to get bookings

--- src/app/bookings/page.tsx
+++ src/app/bookings/page.tsx
@@ -2,48 +2,18 @@ import { getServerSession } from "next-auth";
 import Header from "../_components/header";
 import { authOptions } from "../_lib/auth";
 import { notFound } from "next/navigation";
-import { db } from "../_lib/prisma";
 import BookingItem from "../_components/booking-item";
+import { getConfirmedBookings } from "../_data/get-confirmed-bookings";
+import { getConcluedBookings } from "../_data/get-conclued-bookings";
 
 const Bookings = async () => {
     const session = await getServerSession(authOptions)
     if (!session?.user) {
         return notFound();
     }
-    const confirmedBookings = await db.booking.findMany({
-        where: {
-            userId: (session?.user as any).id,
-            date: {
-                gte: new Date(),
-            }
-        },
-        include: {
-            service: {
-                include: {
-                    barbershop: true,
-                },
-            },
-        },
-        orderBy: {
-            date: "asc",
-        },
-    })
+    const confirmedBookings = await getConfirmedBookings()
 
-    const concludedBookings = await db.booking.findMany({
-        where: {
-            userId: (session?.user as any).id,
-            date: {
-                lt: new Date(),
-            }
-        },
-        include: {
-            service: {
-                include: {
-                    barbershop: true,
-                }
-            }
-        }
-    })
+    const concludedBookings = await getConcluedBookings()
 
     return ( 
 
